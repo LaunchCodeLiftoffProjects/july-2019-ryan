d6bb196aa685afdeeedbaeddf6e5cb9a1d73fe66
Tyler Kelso <kelsotyler11@gmail.com>
2019-08-07 08:06:01 -0500

    Initial Commit

---
+++ liftoff-project/src/main/java/com/example/liftoffproject/LiftoffProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.liftoffproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftoffProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftoffProjectApplication.class, args);
+	}
+
+}
