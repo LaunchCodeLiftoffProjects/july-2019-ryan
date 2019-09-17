b67a3c0b19dacb1ce467d7812cda69f4e19d3797
JaneSewCoder <ajanerain@gmail.com>
2019-09-17 16:42:36 -0500

    new errors

--- src/main/java/com/example/liftoffproject/models/data/GenreDao.java
+++ src/main/java/com/example/liftoffproject/models/data/GenreDao.java
@@ -5,7 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import javax.transaction.Transactional;
 
-
 @Repository
 @Transactional
 public interface GenreDao extends CrudRepository<Genre, Integer> {
--- src/main/java/com/example/liftoffproject/models/forms/Genre.java
+++ src/main/java/com/example/liftoffproject/models/forms/Genre.java
@@ -3,8 +3,6 @@ package com.example.liftoffproject.models.forms;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class Genre {
@@ -16,10 +14,6 @@ public class Genre {
     @NotNull
     @Size(min=3, max=15)
     private String name;
-
-    //@ManyToMany
-    //@JoinColumn(name = "genre_id")
-    private List<Genre> events = new ArrayList<>();
 
     public Genre(){ }
 
